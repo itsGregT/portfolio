2e21f8008e0d17d8a30facd8ea3c49f5a2b46ff2
gregthomas <greg.thomas@voxsomnia.io>
2026-06-05 22:35:14 -0500

    * filling out content * styling and layout updates

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { MailIcon } from 'lucide-react';
+import { Dot, MailIcon } from 'lucide-react';
 import GitHubIcon from "@/components/icons/GithubIcon";
 import LinkedInIcon from "@/components/icons/LinkedinIcon";
 
@@ -8,12 +8,26 @@ export default function Hero() {
         <section id="home" className="hero">
             <div className="hero__grid grid grid--50-50">
                 <div className="hero__left">
-                    <h2 className="hero__subtitle">Frontend Engineer * UX Designer * Product Owner</h2>
+                    <h2 className="hero__subtitle">Frontend Engineer <Dot /> UX Designer <Dot /> Product Owner</h2>
                     <h1 className="hero__title">I build digital products that solve real problems</h1>
                     <p className="hero__description">I combine product thinking, user experience, and modern frontend development to build scalable, data-drive applications that deliver measurable impact.</p>
                     <div className="hero__actions">
                         <a className="button button--primary" href="#work">View My Work</a>
                         <a className="button button--secondary" href="" download>Download Resume</a>
+                    </div>
+                    <div className="hero__links">
+                        <a href="https://github.com/itsGregT" target="_blank" rel="noopener noreferrer">
+                            <p className="visually-hidden">View my GitHub profile.</p>
+                            <GitHubIcon />
+                        </a>
+                        <a href="https://linkedin.com/in/itsgregt" target="_blank" rel="noopener noreferrer">
+                            <p className="visually-hidden">View my LinkedIn profile.</p>
+                            <LinkedInIcon />
+                        </a>
+                        <a href="mailto:gregthomas.dev@gmail.com">
+                            <p className="visually-hidden">Send me an email.</p>
+                            <MailIcon stroke="white" width="45" height="45" />
+                        </a>
                     </div>
                 </div>
                 <div className="hero__right">
@@ -22,25 +36,11 @@ export default function Hero() {
                             className="hero__media" 
                             src="/img/greg.jpg" 
                             alt="Greg Thomas." 
-                            width={500}
-                            height={500}    
+                            width={600}
+                            height={600}    
                         />
                     </div>
                 </div>
-            </div>
-            <div className="hero__links">
-                <a href="https://github.com/itsGregT" target="_blank" rel="noopener noreferrer">
-                    <p className="visually-hidden">View my GitHub profile.</p>
-                    <GitHubIcon />
-                </a>
-                <a href="https://linkedin.com/in/itsgregt" target="_blank" rel="noopener noreferrer">
-                    <p className="visually-hidden">View my LinkedIn profile.</p>
-                    <LinkedInIcon />
-                </a>
-                <a href="mailto:gregthomas.dev@gmail.com">
-                    <p className="visually-hidden">Send me an email.</p>
-                    <MailIcon stroke="yellow" width="32" />
-                </a>
             </div>
         </section>
     )
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,6 +1,19 @@
+import { ExternalLink } from "lucide-react";
 import Image from "next/image";
 
 const projects = [
+    {
+        title: "VoxSomnia Website",
+        desc: "VoxSomnia company website",
+        tags: ["React", "Next.js", "Typescript", "SCSS"],
+        img: "/img/voxsomnia-poster.jpg"
+    },
+    {
+        title: "VoxSomnia Website",
+        desc: "VoxSomnia company website",
+        tags: ["React", "Next.js", "Typescript", "SCSS"],
+        img: "/img/voxsomnia-poster.jpg"
+    },
     {
         title: "VoxSomnia Website",
         desc: "VoxSomnia company website",
@@ -14,28 +27,30 @@ export default function Projects() {
         <section id="work" className="section projects">
             <h2 className="section__title">Featured Projects</h2>
             <p className="section__subtitle">Here are some of my recent works.</p>
-            {projects.map((project, i) => (
-                <div key={i} className="project-tile">
-                    <div className="project-tile__media">
-                        <Image 
-                            src={project.img}
-                            alt={project.title}
-                            className="project-tile__media-img"
-                            width={500}
-                            height={500}
-                        />
-                    </div>
-                    <div className="project-tile__content">
-                        <h3 className="project-tile__title">{project.title}</h3>
-                        <p className="project-tile__description">{project.desc}</p>
-                        <div className="project-tile__tags">
-                            {project.tags.map((tag, j) => (
-                                <span className="project-tile__tag" key={j}>{tag}</span>
-                            ))}
+            <div className="project-tiles">
+                {projects.map((project, i) => (
+                    <div key={i} className="project-tile">
+                        <div className="project-tile__media">
+                            <Image 
+                                src={project.img}
+                                alt={project.title}
+                                className="project-tile__media-img"
+                                width={500}
+                                height={500}
+                            />
+                        </div>
+                        <div className="project-tile__content">
+                            <h3 className="project-tile__title">{project.title} <ExternalLink /></h3>
+                            <p className="project-tile__description">{project.desc}</p>
+                            <div className="project-tile__tags">
+                                {project.tags.map((tag, j) => (
+                                    <span className="project-tile__tag" key={j}>{tag}</span>
+                                ))}
+                            </div>
                         </div>
                     </div>
-                </div>
-            ))}
+                ))}
+            </div>
         </section>
     )
 }
