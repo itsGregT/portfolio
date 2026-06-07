88fa239ed01d59f49461e1fd018647bcfa966331
gregthomas <greg.thomas@voxsomnia.io>
2026-06-07 14:09:34 -0500

    * finished initial projects section * finished initial about section

--- components/About.tsx
+++ components/About.tsx
@@ -1,37 +1,38 @@
+import { ChartNoAxesCombined, Code, Laptop, Users } from "lucide-react";
 
 export default function About() {
     return (
-        <section id="work" className="section projects">
+        <section id="work" className="section projects grid grid--40-60">
             <div className="about__left">
-                <h2 className="section__title">About Me</h2>
-                <p className="section__subtitle">I bridge the gap between business needs and technical solutions.</p>
+                <h2 className="section__subtitle">About Me</h2>
+                <p className="section__title">I bridge the gap between business needs and technical solutions.</p>
                 <p className="section__desc">With 13+ years in design, 10+ years in frontend development and experience as a Product Owner, I bring a unique blend of skills to build products that users love and businesses value.</p>
             </div>
             <div className="about__right">
                 <div className="about-grid">
                     <div className="about-grid__item">
-                        <span className="about-grid__icon">TILE_ICON</span>
+                        <span className="about-grid__icon"><Laptop stroke="white" width="50" height="50" /></span>
                         <p className="about-grid__title">
                             <span className="about-grid__title--lg">13+ years</span>
                             <span className="about-grid__title--sm">In digital design and development</span>
                         </p>
                     </div>
                     <div className="about-grid__item">
-                        <span className="about-grid__icon">TILE_ICON</span>
+                        <span className="about-grid__icon"><Code stroke="white" width="50" height="50" /></span>
                         <p className="about-grid__title">
                             <span className="about-grid__title--lg">10+ years</span>
                             <span className="about-grid__title--sm">Frontend development with modern technologies</span>
                         </p>
                     </div>
                     <div className="about-grid__item">
-                        <span className="about-grid__icon">TILE_ICON</span>
+                        <span className="about-grid__icon"><Users stroke="white" width="45" height="45" /></span>
                         <p className="about-grid__title">
                             <span className="about-grid__title--lg">Product Leader</span>
                             <span className="about-grid__title--sm">Experience as Product Owner and team lead</span>
                         </p>
                     </div>
                     <div className="about-grid__item">
-                        <span className="about-grid__icon">TILE_ICON</span>
+                        <span className="about-grid__icon"><ChartNoAxesCombined stroke="white" width="45" height="45" /></span>
                         <p className="about-grid__title">
                             <span className="about-grid__title--lg">Results Driven</span>
                             <span className="about-grid__title--sm">Delivering measurable impact through technology</span>
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -6,27 +6,27 @@ const projects = [
         title: "VoxSomnia Website",
         desc: "VoxSomnia company website",
         tags: ["React", "Next.js", "Typescript", "SCSS"],
-        img: "/img/voxsomnia-poster.jpg"
+        img: "/img/voxsomnia.jpg"
     },
     {
         title: "VoxSomnia Website",
         desc: "VoxSomnia company website",
         tags: ["React", "Next.js", "Typescript", "SCSS"],
-        img: "/img/voxsomnia-poster.jpg"
+        img: "/img/voxsomnia.jpg"
     },
     {
         title: "VoxSomnia Website",
         desc: "VoxSomnia company website",
         tags: ["React", "Next.js", "Typescript", "SCSS"],
-        img: "/img/voxsomnia-poster.jpg"
+        img: "/img/voxsomnia.jpg"
     }
 ]
 
 export default function Projects() {
     return (
         <section id="work" className="section projects">
-            <h2 className="section__title">Featured Projects</h2>
-            <p className="section__subtitle">Here are some of my recent works.</p>
+            <h2 className="section__subtitle">Featured Projects</h2>
+            <p className="section__title">Here are some of my recent works.</p>
             <div className="project-tiles">
                 {projects.map((project, i) => (
                     <div key={i} className="project-tile">
@@ -35,8 +35,8 @@ export default function Projects() {
                                 src={project.img}
                                 alt={project.title}
                                 className="project-tile__media-img"
-                                width={500}
-                                height={500}
+                                width={536}
+                                height={302}
                             />
                         </div>
                         <div className="project-tile__content">
